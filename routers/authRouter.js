e3cab7d3449b3a0799cd488f45311238f35bbf18
Rahul Dahal <rdaahal@gmail.com>
2020-12-05 12:07:51 +0545

    fix: logout bug

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/User");
+const { User } = require("../models/User");
 
 // logout
 
