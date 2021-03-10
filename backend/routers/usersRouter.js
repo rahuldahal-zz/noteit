02764eeb6859e74128ad9d5e944f135c932aba0a
Rahul Dahal <rdaahal@gmail.com>
2021-03-10 19:29:33 +0545

    fix: npm audit fix
    fix: cleanup backend/app
    
    feat: implemented morgan request logger

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -10,6 +10,11 @@ router.get(
   notesController.sendNotesDescriptionToClient
 );
 
+router.post("/create", (req, res) => {
+  const { firstName, lastName, picture, id, email } = req.body;
+  res.status(202).json({ message: id });
+});
+
 router.post("/saveFacultyAndSemester", userController.saveFacultyAndSemester);
 
 module.exports = router; //exports "router" to the app.js
