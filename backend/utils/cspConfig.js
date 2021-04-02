313c6de6f27da0f122100a95834ca83f07a14875
Rahul Dahal <rdaahal@gmail.com>
2021-04-02 20:37:07 +0545

    fix: CSP now allows content from *.googleusercontent.com
    fix: remove fontawesome cdn link

--- backend/utils/cspConfig.js
+++ backend/utils/cspConfig.js
@@ -4,20 +4,13 @@ const cspConfig = contentSecurityPolicy({
   directives: {
     defaultSrc: [
       "'self'",
-      "https://ka-f.fontawesome.com",
       "https://res.cloudinary.com",
       "https://fonts.gstatic.com",
-      "https://lh3.googleusercontent.com",
+      "https://*.googleusercontent.com",
       "https://platform-lookaside.fbsbx.com",
     ],
-    scriptSrc: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
-    connectSrc: ["'self'", "https://dev-ot7h-cfg.us.auth0.com"],
-    styleSrc: [
-      "'self'",
-      "'unsafe-inline'",
-      "https://ka-f.fontawesome.com",
-      "https://fonts.googleapis.com",
-    ],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
     objectSrc: ["'none'"],
     upgradeInsecureRequests: [],
   },
