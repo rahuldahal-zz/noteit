d9042d417a50fe4471807a0a2b0e72ed59b12234
Rahul Dahal <rdaahal@gmail.com>
2021-04-03 17:58:29 +0545

    feat: pwa caching static assets
    fix: CSP allow google fonts

--- backend/utils/cspConfig.js
+++ backend/utils/cspConfig.js
@@ -5,11 +5,13 @@ const cspConfig = contentSecurityPolicy({
     defaultSrc: [
       "'self'",
       "https://res.cloudinary.com",
+      "https://fonts.googleapis.com/",
       "https://fonts.gstatic.com",
       "https://*.googleusercontent.com",
       "https://platform-lookaside.fbsbx.com",
     ],
     scriptSrc: ["'self'", "'unsafe-inline'"],
+    fontSrc: ["https://fonts.gstatic.com"],
     styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
     objectSrc: ["'none'"],
     upgradeInsecureRequests: [],
--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,6 +1,79 @@
-const CACHE_NAME = "site-static-v0.1";
+const CACHE_NAMES = {
+  static: "static-v0.1",
+  general: "general-v0.1",
+};
+const STATIC_ASSETS = [
+  "/",
+  "/static/main.js",
+  "/static/vendors~main.js",
+  "/static/styles.css",
+  "/static/1.styles.css",
+  "/manifest.json",
+  "https://res.cloudinary.com/rdaahal/image/upload/v1613298797/noteit/images/noteit_j0dofz.png",
+];
 
 self.addEventListener("install", (event) => {
   console.log("installed");
-  self.skipWaiting(); // makes the new service-worker take effect immediately
+  event.waitUntil(
+    caches
+      .open(CACHE_NAMES.static)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .catch((err) => console.log(err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("is activated");
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAMES.static, CACHE_NAMES.general);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+  const { host, href, pathname } = new URL(request.url);
+  const fetchAndCache = [/:\/\/cloudinary/, /fonts.(googleapis|gstatic)/];
+
+  return event.respondWith(
+    caches
+      .match(request)
+      .then((response) => {
+        if (response) {
+          console.log("returning from cache ", pathname);
+          return response;
+        }
+        if (fetchAndCache.some((regex) => regex.test(href))) {
+          console.log("about to fetch and cache");
+          return caches
+            .open(CACHE_NAMES.general)
+            .then((cache) => {
+              return fetch(request)
+                .then((response) => {
+                  console.log("fetching and caching ", href);
+                  cache.put(request, response.clone());
+                  return response;
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
+        }
+
+        console.log("actually fetching... ", href);
+        return fetch(request);
+      })
+      .catch((err) => console.log(err))
+  );
 });
