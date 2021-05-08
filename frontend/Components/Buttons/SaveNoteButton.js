97c290d1c3576cf8203774198305c42461c416a2
Rahul Dahal <rdaahal@gmail.com>
2021-05-08 18:01:48 +0545

    fix: saveNote button state
    fix: UI for rendered note

--- frontend/Components/Buttons/SaveNoteButton.js
+++ frontend/Components/Buttons/SaveNoteButton.js
@@ -1,16 +1,21 @@
 import TextWithIcon from "@components/TextWithIcon";
+import { useAuth } from "@contexts/AuthProvider";
 import useFetch from "@hooks/useFetch";
 import getIconPaths from "@utils/iconDetails";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 
 export default function SaveNoteButton({ noteId }) {
+  const [isSaved, setIsSaved] = useState(false);
+  const { user } = useAuth();
+  const [savedNotes] = useState(user.savedNotes);
   const [startFetching, status, data] = useFetch();
 
   function handleClick() {
-    console.log(`should save ${noteId}`);
+    console.log(`should handle ${noteId}`);
+    const path = isSaved ? "removeSaved" : "save";
     startFetching({
-      url: "/users/notes/save",
+      url: `/users/notes/${path}`,
       options: {
         method: "PUT",
         headers: {
@@ -22,13 +27,23 @@ export default function SaveNoteButton({ noteId }) {
   }
 
   useEffect(() => {
-    console.log({ status });
+    setIsSaved(savedNotes.includes(noteId));
+  }, [noteId]);
+
+  useEffect(() => {
+    console.log({ data });
+    if (data && data.message === "Note is removed successfully!") {
+      setIsSaved(false);
+    }
+    if (data && data.message === "Note is saved successfully!") {
+      setIsSaved(true);
+    }
   }, [status, data]);
 
   return (
     <Button className="note__save" onClick={handleClick}>
       <TextWithIcon
-        textContent="Save Note"
+        textContent={isSaved ? "Remove Saved" : "Save Note"}
         pathData={getIconPaths("bookmark")}
       />
     </Button>
