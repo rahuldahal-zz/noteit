3d64c69f739c4d5a06b79005b3711ff75918b431
Rahul Dahal <rdaahal@gmail.com>
2021-06-04 19:55:20 +0545

    feat: show flash message on save/removal of note

--- frontend/Components/Buttons/SaveNoteButton.js
+++ frontend/Components/Buttons/SaveNoteButton.js
@@ -1,8 +1,10 @@
 import TextWithIcon from "@components/TextWithIcon";
 import { useAuth } from "@contexts/AuthProvider";
 import useFetch from "@hooks/useFetch";
+import useFlash from "@hooks/useFlash";
 import getIconPaths from "@utils/iconDetails";
 import React, { useEffect, useState } from "react";
+import FlashMessage from "@components/FlashMessage";
 import Button from "./Button";
 
 export default function SaveNoteButton({ noteId }) {
@@ -10,6 +12,7 @@ export default function SaveNoteButton({ noteId }) {
   const { user } = useAuth();
   const [savedNotes] = useState(user.savedNotes);
   const [startFetching, status, data] = useFetch();
+  const [flashMessage, setFlashMessage] = useFlash();
 
   function handleClick() {
     console.log(`should handle ${noteId}`);
@@ -38,14 +41,23 @@ export default function SaveNoteButton({ noteId }) {
     if (data && data.message === "Note is saved successfully!") {
       setIsSaved(true);
     }
+    if (data) {
+      setFlashMessage({
+        type: "success",
+        message: data.message,
+      });
+    }
   }, [status, data]);
 
   return (
-    <Button className="note__save" onClick={handleClick}>
-      <TextWithIcon
-        textContent={isSaved ? "Remove Saved" : "Save Note"}
-        pathData={getIconPaths("bookmark")}
-      />
-    </Button>
+    <>
+      <Button className="note__save" onClick={handleClick}>
+        <TextWithIcon
+          textContent={isSaved ? "Remove Saved" : "Save Note"}
+          pathData={getIconPaths("bookmark")}
+        />
+      </Button>
+      <FlashMessage flashDetails={flashMessage} setStateRef={setFlashMessage} />
+    </>
   );
 }
