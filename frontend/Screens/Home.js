1b085f21f21572d60d258cc41d4f032940b444cd
Rahul Dahal <rdaahal@gmail.com>
2021-06-13 11:18:52 +0545

    feat:remove flash message from Home

--- frontend/Screens/Home.js
+++ frontend/Screens/Home.js
@@ -16,10 +16,6 @@ export default function Home() {
     if (!noteContext.isLoading) {
       setAvailableNotes(noteContext);
       setIsLoading(false);
-      setFlashMessage({
-        type: "success",
-        message: "Available notes loaded successfully.",
-      });
     }
   }, [noteContext]);
 
