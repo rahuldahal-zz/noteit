89c90b6ee8efcbf92f9c356ccb9d8b899e30b859
Rahul Dahal <rdaahal@gmail.com>
2021-06-13 17:38:53 +0545

    fix: loading screen

--- frontend/Screens/Home.js
+++ frontend/Screens/Home.js
@@ -5,6 +5,7 @@ import AvailableSubjects from "@components/UserDashboard/AvailableSubjects";
 import { useNote } from "@contexts/NoteProvider";
 import useFlash from "@hooks/useFlash";
 import FlashMessage from "@components/FlashMessage";
+import Loader from "@components/Loader";
 
 export default function Home() {
   const [availableNotes, setAvailableNotes] = useState([]);
@@ -22,11 +23,7 @@ export default function Home() {
   return (
     <Screen>
       <Container className="home">
-        {isLoading ? (
-          <h3>Fetching notes...</h3>
-        ) : (
-          <AvailableSubjects notes={availableNotes} />
-        )}
+        {isLoading ? <Loader /> : <AvailableSubjects notes={availableNotes} />}
       </Container>
       <FlashMessage flashDetails={flashMessage} setStateRef={setFlashMessage} />
     </Screen>
