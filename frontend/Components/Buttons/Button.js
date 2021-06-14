3fac75a154c50a1a621c1044406a52ffb780fc99
Rahul Dahal <rdaahal@gmail.com>
2021-06-14 12:02:50 +0545

    fix: button now preserves its size while showing loader

--- frontend/Components/Buttons/Button.js
+++ frontend/Components/Buttons/Button.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Loader from "@svgs/buttonLoader.svg";
 
 export default function Button({
   children,
-  type,
+  type = "button",
   className,
   onClick: callback,
 }) {
+  const btnRef = useRef(null);
   const [showLoader, setShowLoader] = useState(false);
+  const [size, setSize] = useState(null);
 
   function getClassName() {
     if (showLoader) {
@@ -17,6 +19,8 @@ export default function Button({
   }
 
   function handleClick() {
+    const { width, height } = btnRef.current.getBoundingClientRect();
+    setSize({ width, height });
     setShowLoader(true);
     callback();
   }
@@ -27,6 +31,8 @@ export default function Button({
       className={`${className} ${getClassName()}`}
       onClick={handleClick}
       onBlur={() => setShowLoader(false)}
+      ref={btnRef}
+      style={size ? { width: size.width, height: size.height } : {}}
     >
       {showLoader ? <Loader /> : children}
     </button>
