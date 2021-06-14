85a65a618681bb4cac8515b549186c4cb3319052
Rahul Dahal <rdaahal@gmail.com>
2021-06-14 10:48:46 +0545

    fix: clean up following 1b085f21f21572d60d258cc41d4f032940b444cd

--- frontend/Screens/Home.js
+++ frontend/Screens/Home.js
@@ -3,14 +3,11 @@ import Container from "@components/Container";
 import Screen from "@components/Screen";
 import AvailableSubjects from "@components/UserDashboard/AvailableSubjects";
 import { useNote } from "@contexts/NoteProvider";
-import useFlash from "@hooks/useFlash";
-import FlashMessage from "@components/FlashMessage";
 import Loader from "@components/Loader";
 
 export default function Home() {
   const [availableNotes, setAvailableNotes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [flashMessage, setFlashMessage] = useFlash();
   const noteContext = useNote();
 
   useEffect(() => {
@@ -25,7 +22,6 @@ export default function Home() {
       <Container className="home">
         {isLoading ? <Loader /> : <AvailableSubjects notes={availableNotes} />}
       </Container>
-      <FlashMessage flashDetails={flashMessage} setStateRef={setFlashMessage} />
     </Screen>
   );
 }
