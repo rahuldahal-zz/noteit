8d0d07520756655e47cf6a9bf588fdae23aa63bc
Rahul Dahal <rdaahal@gmail.com>
2021-06-17 10:48:59 +0545

    fix:style wideScreen UI for units

--- frontend/Components/UserDashboard/AvailableSubjects.js
+++ frontend/Components/UserDashboard/AvailableSubjects.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Modal from "@components/Modal";
 import Units from "./Units";
+import isScreenLargerThan from "@utils/screenSize";
 
 export default function AvailableSubjects({ notes }) {
   const [subjects] = useState(Object.keys(notes));
   const [showUnits, setShowUnits] = useState(false);
   const [currentSubject, setCurrentSubject] = useState(null);
+  const [isScreenWide, setIsScreenWide] = useState(false);
+
+  useEffect(() => {
+    if (isScreenLargerThan(768)) {
+      setIsScreenWide(true);
+    }
+  }, []);
 
   function handleClick(subject) {
     setShowUnits(true);
@@ -34,7 +42,7 @@ export default function AvailableSubjects({ notes }) {
       <Modal
         shouldOpen={showUnits}
         classToToggle={{
-          modal: "modal--units",
+          modal: `${isScreenWide ? "modal--wide" : "modal--units"}`,
           child: "home__units-wrapper--active",
         }}
         setStateRef={setShowUnits}
